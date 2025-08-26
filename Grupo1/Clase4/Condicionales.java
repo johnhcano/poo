44f72e1f96ac506ebdac12df6f712fbd93ef2991
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-08-26 13:53:54 +0000

    condicionales

---
+++ Grupo1/Clase4/Condicionales.java
@@ -0,0 +1,17 @@
+public class Condicionales {
+    public static void main(String[] args) {
+
+        // Problema1: Se necesita validar la edad de una persona mayor de edad (En
+        // Colombia la mayoría de edad se obtiene al cumplir los 18 años)
+
+        // Se declara la variable edad de tipo entero
+        int edad = 18;
+
+        if (edad >= 18) {
+            System.out.println("Mayor de edad");
+        } else {
+            System.out.println("No es mayor de edad");
+        }
+
+    }
+}
