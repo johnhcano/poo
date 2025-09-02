f1957ec58bcd59542287701adbd3fa853f4e89af
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-02 14:00:43 +0000

    cliclos

---
+++ Grupo1/Clase5/EstructurasRepeticionCiclos.java
@@ -0,0 +1,22 @@
+public class EstructurasRepeticionCiclos {
+    public static void main(String[] args) {
+        
+        //for (para)
+        //for(inicio; condición; incremento)
+        for(int i = 1; i <= 5; i++){
+            System.out.println("i = " + i);
+        }
+
+        /*
+         * Prueba de escritorio
+         * i    i <= 5      i++     "i = " + i
+         * 1    1 <= 5 V    2        i = 1
+         *      2 <= 5 V    3        i = 2
+         *      3 <= 5 V    4        i = 3
+         *      4 <= 5 V    5        i = 4
+         *      5 <= 5 V    6        i = 5
+         *      6 <= 5 F X
+         */
+
+    }
+}
