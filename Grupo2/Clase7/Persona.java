d553e8f9e70020e308b4b977f49f4247dab22ee3
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-17 21:53:07 +0000

    update

--- Grupo2/Clase7/Persona.java
+++ Grupo2/Clase7/Persona.java
@@ -8,6 +8,18 @@ public class Persona {
     private double estatura;
     private double peso;
 
+    //Método constructor de la clase se reconoce porque tiene el mismo nombre de la clase
+    //El constructor de la clase me permite inicializar la clase
+    //la palabra reservada "this" se utiliza para autoreferenciar el atributo de la clase
+    public Persona(int identificacion, String nombres, String apellidos, int edad, double estatura, double peso){
+       this.identificacion = identificacion;
+       this.nombres = nombres;
+       this.apellidos = apellidos;
+       this.edad = edad;
+       this.estatura = estatura;
+       this.peso = peso;
+    }
+
     //Métodos
     public void caminar(){}
     public boolean jugar(){}
